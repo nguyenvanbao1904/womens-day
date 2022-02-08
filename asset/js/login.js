ce68e95ee1a7e79da8d1c92d94d1bae298edc067
nguyenvanbao <bao19042004@gmail.com>
2022-02-08 21:13:54 +0700

    fix

--- asset/js/login.js
+++ asset/js/login.js
@@ -38,46 +38,52 @@ function updateStatus(id,data){
     fetch(usersApi+'/'+id,options)
 }
 
+function checkLogin(){
+    var userNameInput = document.querySelector('#userName').value   
+    var passwordInput = document.querySelector('#password').value
+    var h2 = document.querySelector('h2')
+    var userNameOrPassword = document.querySelector('.user-name-or-password-small')
+    getUsers(function(users){
+        getPasswords(function(passwords){
+            var userName = users.find(function(user){
+                return user.userName.includes(userNameInput)
+            })
+           var pass = passwords.find(function(password){
+                return password.id===userName.id
+           })
+            if(userName.userName==userNameInput && pass.password==passwordInput){
+                userNameOrPassword.innerText = ''
+                var data = {
+                    "id": userName.id,
+                    "userName": userName.userName,
+                    "status":"Ok"
+                }
+                function nextPage(){
+                    window.location.href =`${userName.userName}.html`
+                }
+                updateStatus(userName.id,data)
+                setTimeout(nextPage,1000)
+            } else{
+                alertError(userNameOrPassword,'Tài Khoản hoặc mật khẩu không chính xác !!')
+            }
+        })
+        var checkUser = users.some(function(user){
+            return user.userName==userNameInput
+        })
+        if(!checkUser){
+            alertError(userNameOrPassword,'Tài Khoản hoặc mật khẩu không chính xác !!')
+        }
+    })
+}
+
 function check (){
     var submitBtn = document.querySelector('.submit-btn')
     if(submitBtn){
-        submitBtn.onclick=function(){
-            var userNameInput = document.querySelector('#userName').value   
-            var passwordInput = document.querySelector('#password').value
-            var h2 = document.querySelector('h2')
-            var userNameOrPassword = document.querySelector('.user-name-or-password-small')
-            getUsers(function(users){
-                getPasswords(function(passwords){
-                    console.log(users)
-                    var userName = users.find(function(user){
-                        return user.userName.includes(userNameInput)
-                    })
-                   var pass = passwords.find(function(password){
-                        return password.id===userName.id
-                   })
-                    if(userName.userName==userNameInput && pass.password==passwordInput){
-                        userNameOrPassword.innerText = ''
-                        var data = {
-                            "id": userName.id,
-                            "userName": userName.userName,
-                            "status":"Ok"
-                        }
-                        function nextPage(){
-                            window.location.href =`${userName.userName}.html`
-                        }
-                        updateStatus(userName.id,data)
-                        setTimeout(nextPage,1000)
-                    } else{
-                        alertError(userNameOrPassword,'Tài Khoản hoặc mật khẩu không chính xác !!')
-                    }
-                })
-                var checkUser = users.some(function(user){
-                    return user.userName==userNameInput
-                })
-                if(!checkUser){
-                    alertError(userNameOrPassword,'Tài Khoản hoặc mật khẩu không chính xác !!')
-                }
-            })
+        submitBtn.addEventListener('click',checkLogin)
+    }
+    document.onkeydown = function(e){
+        if(e.which===13){
+            checkLogin()
         }
     }
 }
